9b365a8d95c12468699b98a44e1b98c8e1193ae2
hoang9xvn113 <79950372+hoang9xvn113@users.noreply.github.com>
2021-06-01 20:55:05 +0700

    Delete backend directory

--- backend/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    define("DB_SERVER", "localhost");
-    define("DB_USER", "id15708751_root");
-    define("DB_PASS", "ieFHgv[L[Hf]?3IT");
-    define("DB_NAME", "id15708751_bookre");
-?>
