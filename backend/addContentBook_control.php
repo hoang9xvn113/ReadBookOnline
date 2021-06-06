a2de78e7f2ff37fdd9322704e45df88e0a74489f
hoang9xvn113 <79950372+hoang9xvn113@users.noreply.github.com>
2021-06-06 08:41:57 +0700

    Update addContentBook_control.php

--- backend/addContentBook_control.php
+++ backend/addContentBook_control.php
@@ -3,6 +3,7 @@
     
     $db = new Database();
     $book_id = $page = $content = "";
+
     if ($_SERVER['REQUEST_METHOD'] == 'GET'){
         if (isset($_GET['book_id'])){
             $book_id = $_GET['book_id'];
@@ -14,23 +15,29 @@
             $content = $_GET['content'];
         }
         
+        
         $insert_content = "insert into content(book_id, page, content) values($book_id, $page, '$content')";
         if ($book_id != ""){
-            $result = $db->insert($insert_content);
-            echo "<h1>";
-            if ($result){
-                $update = "update book set amount=amount+1 where id=$book_id";
-                $result_update = $db->update($update);
-                if ($result_update) {
-                    echo "Thêm nội dung trang thành công";
+            if ($content == "" || $page == "") {
+                echo "Vui lòng điền đầy đủ thông tin";
+            } else {
+                $result = $db->insert($insert_content);
+                echo "<h1>";
+                if ($result){
+                    $update = "update book set amount=amount+1 where id=$book_id";
+                    $result_update = $db->update($update);
+                    if ($result_update) {
+                        echo "Thêm nội dung trang thành công";
+                    }
+                    
+                } else{
+                    echo "Thêm nội dung trang thất bại";
                 }
-                
-            } else{
-                echo "Thêm nội dung trang thất bại";
+                echo "</h1>";
             }
-            echo "</h1>";
-            echo "<a href='https://hochoihamhoc.000webhostapp.com/'>Trở lại trang chủ</a>";
-        }
+        } 
+        
+        echo "<a href='https://hochoihamhoc.000webhostapp.com/'>Trở lại trang chủ</a>";
     }
 
 ?>
